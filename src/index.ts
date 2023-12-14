ac6b84ad3bd54122012eb1c29e5c4ca04364eba4
Tomek Marciniak <tmarciniakm@gmail.com>
2023-12-14 17:15:02 +0100

    feat(path-gpt): add pathnames

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import { globby } from "globby";
 import { readFile, writeJson } from "fs-extra";
 import ignoreFiles from "./ignore.json";
+import path from "path";
 
 const run = async () => {
+  const currentPath = process.cwd();
   const ignoreArr = ignoreFiles.map((file) => [`!${file}`, `!**/*/${file}`]);
   const filesInCwd = await globby(["**/*", ...ignoreArr].flat(), {
     gitignore: true,
-    absolute: true,
+    absolute: false,
   });
   const contents = await Promise.all(
     filesInCwd.map(async (dir) => {
-      const content = await readFile(dir, { encoding: "utf8" });
+      const content = await readFile(path.join(currentPath, dir), {
+        encoding: "utf8",
+      });
       return {
         filename: dir.split("/").slice(-1)[0],
+        pathname: dir,
         content,
       };
-    })
+    }),
   );
   writeJson("./out.json", contents);
 };
